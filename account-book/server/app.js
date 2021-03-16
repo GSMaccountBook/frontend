aaf0d2af62523506e7f05ec3de15e12ca4990a6c
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-03-16 21:14:05 +0900

    add mysql package
    add mysql package and test code but fail

--- account-book/server/app.js
+++ account-book/server/app.js
@@ -3,17 +3,7 @@ var app = express();
 const port = 3002;
 //cors필요하면 넣기
 //const bodyParser = require("body-parser");
-const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host:"127.0.0.1",
-    port:3306,
-    user: "root",
-    password:"0112",
-    database:"accountbook"
-});
-
-connection.connet();
 
 //app.use(bodyParser.urlencoded({extended: false}));
 
--- account-book/server/routes/index.js
+++ account-book/server/routes/index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host:"127.0.0.1",
+    port:3306,
+    user: "root",
+    password:"0112",
+    database:"accountbook"
+});
+
+connection.connect();
+
 router.get('/', function (req, res, next) {
-  res.json([
-    { id: 1, username: "VictorOladipo" },
-    { id: 2, username: "RussellWstbrook" },
-  ]);
+  console.log('get success');
+  connection.query("INSERT INTO test VALUES ('idval,test1val,test2val')",
+	function(error, result, fields) {
+		if(error) {
+			console.log('insert fail');
+		} else {
+			console.log('insert success');
+		}
+	});
 })
 
 module.exports = router;
