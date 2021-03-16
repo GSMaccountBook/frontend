241e08ec45d2d5489af9d899d2077ccccc135b83
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-03-16 09:08:19 +0900

    db
    db

--- account-book/server/app.js
+++ account-book/server/app.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var app = express();
+const port = 3002;
+//cors필요하면 넣기
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host:"127.0.0.1",
+    port:3306,
+    user: "root",
+    password:"0112",
+    database:"accountbook"
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 var indexRouter = require('./routes/index');
 //var userRouter = require('./routes/user');
@@ -7,7 +21,7 @@ var indexRouter = require('./routes/index');
 app.use(indexRouter);
 
 // portnumber를 3002로 지정
-const port = 3002;
+
 
 // 3002번 포트넘버를 가진 서버 생성
 app.listen(port, () => console.log(`listening on port ${port}!`));
